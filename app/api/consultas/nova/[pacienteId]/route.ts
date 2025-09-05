028deabd640a7b8b513730efa9ecc3dca8fe81e0
devmcibot <devmcibot@gmail.com>
2025-09-05 16:36:53 -0300

    Update route.ts

--- app/api/consultas/nova/[pacienteId]/route.ts
+++ app/api/consultas/nova/[pacienteId]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../../src/lib/prisma";
 
-export async function POST(_: Request, { params }: { params: { pacienteId: string }}) {
+export async function POST(_: Request, { params }: { params: { pacienteId: any }}) {
   const pacienteId = Number(params.pacienteId);
   await prisma.consulta.create({ data: { pacienteId }});
   return NextResponse.redirect(new URL(`/medico/${pacienteId}`, process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"));
